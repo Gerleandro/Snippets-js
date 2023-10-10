cc84f220c6cd4035339d623e245a55f49d8d61ac
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-10-10 17:12:27 -0300

    Snippets-js

---
+++ contador-numerico.js
@@ -0,0 +1,9 @@
+var valor = parseFloat(prompt("Digite o valor"));
+var salto = parseFloat(prompt("Digite o valor do salto"));
+var conte = 0;
+
+while (conte <= valor) {
+    console.log(conte = conte + salto)
+}
+
+alert("fim " + (conte - salto))
